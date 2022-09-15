1b6aeba8b55175da1cb4da0655860efa32c6bafc
Solomanm <mtileni.proud@gmail.com>
2022-09-15 12:02:00 +0200

    initial commit

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-
+const MY_FAVORITE_FOOD = process.env.NAME;
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log("Microservices rock!");
+    console.log(MY_FAVORITE_FOOD, 'rock!');
     await sleep(5000);
   }
 }
